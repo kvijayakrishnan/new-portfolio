11aa9d39cd62d0882a4850fde482ecb7ded39acd
kvijayakrishnan <vijayakrishnankbv@yahoo.in>
2024-02-13 14:49:23 +0530

    update the mobile screen

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -40,7 +40,7 @@ function Portfolio() {
                 <p className='py-6 text-xl font-thin'>Take a look at some of my work right here.</p>
             </div>
 
-            <div className=' grid sm:grid-cols-2 md:grid-cols-3 gap-8 sm:px-0'>
+            <div className=' grid sm:grid-cols-2 md:grid-cols-3 gap-8 sm:px-0 '>
                 {portfolios.map(({id, src, demo, code})=>(
                     <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
                         <img src={src}
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -94,28 +94,29 @@ const Skill = () => {
     >
         <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
             <div>
-                <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline capitalize '>
+                <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline capitalize sm:mt '>
                     skills
                 </p>
                 <p className='py-6 text-xl font-thin'>
                     Explore some of my skills here.
                 </p>
-            </div>
-
-            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-8 text-center py-8 px-12 sm:px-0'>
+                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-8 text-center  px-12 sm:px-0'>
                 {skills.map(({id, src, title, style}) =>(
                     
                     <div 
                         key={id}
                         className={` text-xl font-thin shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
                     >
-                        <img src={src} alt='' className='w-5 mx-auto' />
-                        <p className='mt-4'>{title}</p>
+                        <img src={src} alt='' className='w-10 mx-auto' />
+                        <p className='mt-2'>{title}</p>
                     </div>
 
                 ))}
                 
             </div>
+            </div>
+
+            
         </div>
     </div>
   )
